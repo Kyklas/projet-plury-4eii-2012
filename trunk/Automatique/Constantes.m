3861b081e3e4852fdabaa56d05fd676047b8739e
StanislasBertrand@gmail.com <StanislasBertrand@gmail.com@dc1e5285-6839-8fbc-f777-bd44315362bb>
2012-02-14 21:44:07 +0000

    Mise à jour donnée matlab

--- trunk/Automatique/Constantes.m
+++ trunk/Automatique/Constantes.m
@@ -1,6 +1,15 @@
+
+% Paramètres du Système
+
+% Modèle Electrique de la Bobine
 R=2.24; %Ohm
 L=1.49e-2; %H
 
+% Modèle d'interaction mécanique de la bobine
 m=0.1414; %Kg
-ki=0.3; %N.A-1
-kx=334; %N.m-1
+ki=1.375; %N.A-1
+kx=117.3; %N.m-1
+
+% Modèle capteur effet Hall
+Kc_Hall=92; 
+
