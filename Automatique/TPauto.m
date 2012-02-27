73a01c907611e959a5f135aff00d02d64dcc7c91
StanislasBertrand@gmail.com <StanislasBertrand@gmail.com@dc1e5285-6839-8fbc-f777-bd44315362bb>
2012-02-27 21:05:35 +0000

    Modification du rapport d'auto

--- Automatique/TPauto.m
+++ Automatique/TPauto.m
@@ -33,15 +33,15 @@ Cor_av=tf([Tav 1],[0.1*Tav 1])
 disp('Equation du Système (Elec + Meca + Hall)');
 Sys = Sys_elec * Sys_meca*Kc_Hall
 
-%   nichols(Sys);
-%   hold on;
-%  
+   nichols(Sys);
+   hold on;
+ 
  disp('Equation du Système Corrigé');
  Sys_Corr = 27*Sys*Cor_av 
  
-%  nichols(Sys_Corr);
-%  ngrid;
-%  hold off;
+  nichols(Sys_Corr);
+  ngrid;
+  hold off;
 % 
 % title('Diagrammes de Black Système et Système Corrigé');
 % axis([-360,0,-100,20]);  
@@ -66,8 +66,8 @@ Sys = Sys_elec * Sys_meca*Kc_Hall
 % Version Numérique
 Felec = 1/(2*pi*Telec);
 Fav = 1/(2*pi*0.1*Tav);
-Te = 1/(Felec*24);
-Tav = 1/(Fav*6);
+Te = 1/(Felec*12);
+Tav = 1/(Fav*12);
 
 Sys_d = C2d(Sys,Te,'ZOH')
 
