32497f5509bb5089c5864c180c8e42620a459b5b
r.desille@gmail.com <r.desille@gmail.com@dc1e5285-6839-8fbc-f777-bd44315362bb>
2012-03-12 16:54:41 +0000

    Ajout du projet MPLABX

---
+++ trunk/Micro/ProjetAnalogique.X/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Romain
+ *
+ * Created on 12 mars 2012, 17:52
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
---
+++ trunk/Micro/ProjetAnalogique.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=ProjetAnalogique.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/ProjetAnalogique.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=projetanalogique.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/projetanalogique.x.tar
