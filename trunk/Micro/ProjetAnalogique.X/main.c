a194f45dec90902887a62c71acfaf2f8eb5e062e
xavier.galzin@gmail.com <xavier.galzin@gmail.com@dc1e5285-6839-8fbc-f777-bd44315362bb>
2012-04-01 14:10:10 +0000

    Test du timer : fonctionnel avec quelques détails à vérifier Test de l'adc : manque connexion du potentiomètre1 au µc pour tester

--- trunk/Micro/ProjetAnalogique.X/main.c
+++ trunk/Micro/ProjetAnalogique.X/main.c
@@ -8,6 +8,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <p24FV32KA301.h>
+#include "IO.h"
+#include "PLL.h"
+#include "PWM.h"
+#include "ADC.h"
+#include "TIMER1.h"
+
+//variables globales
+unsigned int resultat=0;
+
+//pgr d'interruptions
+
+void __attribute__((__interrupt__, __shadow__)) _T1Interrupt(void)
+{
+/* Interrupt Service Routine code goes here */
+    
+IFS0bits.T1IF = 0; //Reset Timer1 interrupt flag and Return from ISR
+PORTBbits.RB8=!PORTBbits.RB8;
+resultat=ADC_Convert(POT1);
+if(resultat<127){PORTBbits.RB9=0;}
+else {PORTBbits.RB9=1;}
+}
 
 
 // Bits de configuration
@@ -19,11 +40,7 @@ _FWDT(FWDTEN_OFF);
 _FPOR(BOREN_BOR3 & LVRCFG_OFF & PWRTEN_OFF & I2C1SEL_PRI & BORV_V30 & MCLRE_ON);
 _FICD(ICS_PGx2); // DEBUG_ON
 
-#include "IO.h"
-#include "PLL.h"
-#include "PWM.h"
-#include "ADC.h"
-#include "TIMER1.h"
+
 
 /*
  * main
@@ -37,11 +54,8 @@ int main(void)
     TIMER1_Init();
     ADC_Init();
     while(1)
-    {
-        unsigned int resultat;
-        resultat=ADC_Convert(POT1);
-        if(resultat<127){PORTBbits.RB9=0;}
-        else {PORTBbits.RB9=1;}
+    { 
+       
     }
 
     return (EXIT_SUCCESS);
